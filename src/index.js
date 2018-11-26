6ef0edc866f8a0b207708ab7abbb65d78f78467c
Kurt Lekanger <kurt@lekanger.no>
2018-11-26 20:19:33 +0000

    Added starter code - recipe app from Udemy - initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import RecipeApp from './RecipeApp';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<RecipeApp />, document.getElementById('root'));
+registerServiceWorker();
